a2e9cf96c48aa501d154291ad2849726fe474df7
unknown <mano497>
2022-12-17 16:31:18 +0100

    frontend commit

---
+++ src/app/orders/orders.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {ActivatedRoute, Router} from "@angular/router";
+
+@Component({
+  selector: 'app-orders',
+  templateUrl: './orders.component.html',
+  styleUrls: ['./orders.component.css']
+})
+export class OrdersComponent implements OnInit {
+
+  orders:any;
+  customerId!:number;
+
+  constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute) {
+    this.customerId=route.snapshot.params['customerId'];
+  }
+
+  ngOnInit(): void {
+    this.http.get("http://localhost:9999/BILLING-SERVICE/bills/search/byCustomerId?projection=fullBill&customerId="+this.customerId)
+      .subscribe({
+      next: (data)=>{
+       this.orders=data;
+
+      },
+      error: (err)=>{
+
+      }
+    });
+  }
+
+  getOrderDetails(o: any) {
+    this.router.navigateByUrl("/order-details/"+o.id);
+
+  }
+}
